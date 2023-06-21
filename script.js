31340c96978995d48a0026800586eccff51a3e86
Vatsal-Rajput <124786081+Vatsal-Rajput@users.noreply.github.com>
2023-06-21 22:18:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateClickbait() {
+    var clickbaitText = "Clickbait text goes here";
+    var clickbaitBox = document.getElementById("clickbaitBox");
+    clickbaitBox.innerHTML = clickbaitText;
+  }
+  
